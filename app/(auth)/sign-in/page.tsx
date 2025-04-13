59dbe1566bd7733b0d5bd40e9b47ae8d9e92d281
bittucse <bittucse@outlook.com>
2025-04-13 16:02:39 +0530

    update Auth_Ui

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import AuthForm from "@/components/AuthForm";
 
 const Page = () => {
     return (
-        <div>Page</div>
+
+        <AuthForm type="sign-in"/>
     )
 }
 export default Page
